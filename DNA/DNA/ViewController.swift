085fcd0ae88480de6f55c0bdca524733666872e5
jangseoyoung <jangseoyoung0402@gmail.com>
2021-02-05 23:54:05 +0900

    Create : DNA
    프로젝트 생성

---
+++ DNA/DNA/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DNA
+//
+//  Created by 장서영 on 2021/02/05.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
