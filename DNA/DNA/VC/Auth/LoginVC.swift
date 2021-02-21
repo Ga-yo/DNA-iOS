a507ae5327738475e36508bdc7781f7e04ab013c
jangseoyoung <jangseoyoung0402@gmail.com>
2021-02-21 23:56:36 +0900

    Update : UI

--- DNA/DNA/VC/Auth/LoginVC.swift
+++ DNA/DNA/VC/Auth/LoginVC.swift
@@ -9,10 +9,24 @@ import UIKit
 
 class LoginVC: UIViewController {
 
+    @IBOutlet weak var circle: UIView!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var pwTxt: UITextField!
+    @IBOutlet weak var warningLabel : UILabel!
+    @IBOutlet weak var signInButton: UIButton!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        circle.layer.cornerRadius = 0.5 * circle.bounds.size.width
+        emailTxt.layer.cornerRadius = 22
+        pwTxt.layer.cornerRadius = 22
+        signInButton.layer.cornerRadius = 22
+        warningLabel.isHidden = true
+    }
+    
+    @IBAction func signInButton(_ sender: UIButton!){
+        
     }
     
 
