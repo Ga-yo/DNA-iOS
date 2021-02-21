da9222aff966a3848d239276708117453fb4f15e
jangseoyoung <jangseoyoung0402@gmail.com>
2021-02-21 23:56:33 +0900

    Update : UI

--- DNA/DNA/VC/Auth/SignUpVC.swift
+++ DNA/DNA/VC/Auth/SignUpVC.swift
@@ -8,13 +8,36 @@
 import UIKit
 
 class SignUpVC: UIViewController {
+    
+    @IBOutlet weak var circle: UIView!
+    @IBOutlet weak var nameTxt: UITextField!
+    @IBOutlet weak var emailTxt: UITextField!
+    @IBOutlet weak var confirmEmailButton: UIButton!
+    @IBOutlet weak var warningLabel: UILabel!
+    @IBOutlet weak var pwTxt: UITextField!
+    @IBOutlet weak var confirmPwTxt: UITextField!
+    @IBOutlet weak var signUpButton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        circle.layer.cornerRadius = 0.5 * circle.bounds.size.width
+        nameTxt.layer.cornerRadius = 22
+        emailTxt.layer.cornerRadius = 22
+        confirmEmailButton.layer.cornerRadius = 12
+        pwTxt.layer.cornerRadius = 22
+        confirmPwTxt.layer.cornerRadius = 22
+        signUpButton.layer.cornerRadius = 22
+        warningLabel.isHidden = true
     }
     
+    @IBAction func confirmEmailButton(_ sender: UIButton){
+        
+    }
+    
+    @IBAction func signUpButton(_ sender: UIButton){
+        
+    }
 
     /*
     // MARK: - Navigation
