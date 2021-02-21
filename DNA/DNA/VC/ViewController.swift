1101f266f9bbc03ac3cffc17e6fbce9bf3dcb594
jangseoyoung <jangseoyoung0402@gmail.com>
2021-02-21 23:56:09 +0900

    Update : UI

--- DNA/DNA/VC/ViewController.swift
+++ DNA/DNA/VC/ViewController.swift
@@ -8,11 +8,17 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    @IBOutlet weak var signInButton: UIButton!
+    @IBOutlet weak var signUpBUtton: UIButton!
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        signInButton.layer.cornerRadius = 22
+        signUpBUtton.layer.cornerRadius = 22
     }
+    
 
 
 }
