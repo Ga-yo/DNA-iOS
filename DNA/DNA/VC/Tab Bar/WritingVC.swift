06141956d41ace41ddfc690807dcb02fc27261db
jangseoyoung <jangseoyoung0402@gmail.com>
2021-03-02 20:24:54 +0900

    Update : WritingVC

--- DNA/DNA/VC/Tab Bar/WritingVC.swift
+++ DNA/DNA/VC/Tab Bar/WritingVC.swift
@@ -9,7 +9,7 @@ import UIKit
 import DropDown
 
 class WritingVC: UIViewController {
-
+    
     @IBOutlet weak var backgroundView: UIView!
     @IBOutlet weak var SetCategoryButton: UIButton!
     @IBOutlet weak var titleTxt: UITextField!
@@ -27,28 +27,32 @@ class WritingVC: UIViewController {
         backregist.layer.cornerRadius = 16
     }
     
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        self.view.endEditing(true)
+    }
+    
     @IBAction func setCategoryButton(_ sender: UIButton){
         let dropDown = DropDown()
         dropDown.dataSource = ["T - 대리구매자 구하기", "G - 잠수탄 친구 찾기", "C - 일반 대화 하기", "A - 노동자 구하기"]
-//        dropDown.anchorView = sender
-//        dropDown.bottomOffset = CGPoint(x: 0, y: sender.frame.size.height)
-//        dropDown.cellHeight = 28
-//        dropDown.textFont = UIFont.systemFont(ofSize: 14)
+        dropDown.anchorView = sender
+        dropDown.bottomOffset = CGPoint(x: 0, y: sender.frame.size.height)
+        dropDown.cellHeight = 28
+        dropDown.textFont = UIFont.systemFont(ofSize: 14)
         dropDown.show()
-       
+        
         dropDown.selectionAction = { [unowned self] (index: Int, item: String) in
             SetCategoryButton.setTitle("\(item)", for: .normal)
         }
     }
-
+    
     /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
+     // MARK: - Navigation
+     
+     // In a storyboard-based application, you will often want to do a little preparation before navigation
+     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+     // Get the new view controller using segue.destination.
+     // Pass the selected object to the new view controller.
+     }
+     */
+    
 }
