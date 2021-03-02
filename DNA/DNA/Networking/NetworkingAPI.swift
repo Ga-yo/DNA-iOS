86cf286b0ad84659912b31502e480fbadcf61ceb
jangseoyoung <jangseoyoung0402@gmail.com>
2021-03-02 20:25:01 +0900

    Update : Networking

--- DNA/DNA/Networking/NetworkingAPI.swift
+++ DNA/DNA/Networking/NetworkingAPI.swift
@@ -9,9 +9,54 @@ import Foundation
 import Alamofire
 
 enum NetworkingAPI{
+    case refreshToken
     case ConfirmEmail(_ isCorrect: Bool)
     case SignUp(_ name: String, _ email: String, _ password: String)
     case Login(_ email: String, _ password: String)
     case CommentList(_ size: Int, _ page: Int)
     case CommentWr(_ timelineId: Int, _ content: String)
+    
+    var path: String {
+        switch self {
+        case .Login :
+            return "/auth"
+        case .SignUp :
+            return "/signup"
+        case .ConfirmEmail :
+            return "/email"
+        case .CommentList, .CommentWr :
+            return "/comment"
+        }
+    }
+    
+    var headers: HTTPHeaders? {
+        switch self {
+        case .Login, .SignUp:
+            return nil
+        case .refreshToken :
+            let refreshToken : String = "token"
+            let userDefault = UserDefaults.standard
+            userDefault.set(refreshToken, forKey: "refresh-Token")
+            userDefault.synchronize()
+            guard let token = userDefault.string(forKey: "refresh-Token") else {return nil}
+            return ["Authorization" : "Bearer" + "token"]
+            
+        default:
+            
+        }
+    }
+    
+    var parameters: [String : Any] {
+        switch self {
+        case .Login(let email, let password):
+            print(["email":email, "password":password])
+            return ["email":email, "password":password]
+        
+        case .SignUp(let name, let email, let password):
+            print(["name":name, "email":email, "password":password])
+            return ["name":name, "email":email, "password":password]
+        default:
+            return [:]
+        }
+    }
 }
--- DNA/DNA/Networking/httpClient.swift
+++ DNA/DNA/Networking/httpClient.swift
@@ -9,5 +9,30 @@ import Foundation
 import Alamofire
 
 protocol HTTPCleintProvider {
+    func get(_ api: NetworkingAPI) -> DataRequest
+    func post(_ api: NetworkingAPI) -> DataRequest
+    func put(_ api: NetworkingAPI) -> DataRequest
+    func delete(_ api: NetworkingAPI) -> DataRequest
+}
+
+class HTTPClient: HTTPCleintProvider {
+    let baseURL = "http://211.38.86.92:8080"
+    
+    func get(_ api: NetworkingAPI) -> DataRequest {
+        return AF.request(baseURL + api.path, method: .get, parameters: api.parameters, encoding: JSONEncoding.prettyPrinted, headers: api.headers, interceptor: nil)
+    }
+    
+    func post(_ api: NetworkingAPI) -> DataRequest {
+        return AF.request(baseURL + api.path, method: .post, parameters: api.parameters, encoding: URLEncoding.default, headers: api.headers, interceptor: nil)
+    }
+    
+    func put(_ api: NetworkingAPI) -> DataRequest {
+        return AF.request(baseURL + api.path, method: .put, parameters: api.parameters, encoding: JSONEncoding.prettyPrinted, headers: api.headers, interceptor: nil)
+    }
+    
+    func delete(_ api: NetworkingAPI) -> DataRequest {
+        return AF.request(baseURL + api.path, method: .delete, parameters: api.parameters, encoding: JSONEncoding.prettyPrinted, headers: api.headers, interceptor: nil)
+    }
+    
     
 }
