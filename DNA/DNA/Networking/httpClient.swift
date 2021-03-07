e8a1132648263281bb0d72fb6df58a2643c91750
jangseoyoung <jangseoyoung0402@gmail.com>
2021-03-07 21:17:08 +0900

    Update : httpClient

--- DNA/DNA/Networking/httpClient.swift
+++ DNA/DNA/Networking/httpClient.swift
@@ -19,7 +19,7 @@ class HTTPClient: HTTPCleintProvider {
     let baseURL = "http://211.38.86.92:8080"
     
     func get(_ api: NetworkingAPI) -> DataRequest {
-        return AF.request(baseURL + api.path, method: .get, parameters: api.parameters, encoding: JSONEncoding.prettyPrinted, headers: api.headers, interceptor: nil)
+        return AF.request(baseURL + api.path, method: .get, parameters: api.parameters, encoding: URLEncoding.default, headers: api.headers, interceptor: nil)
     }
     
     func post(_ api: NetworkingAPI) -> DataRequest {
