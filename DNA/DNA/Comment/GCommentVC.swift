b509c054badb01470bbf5ca958aa7a2afbdb281e
jangseoyoung <jangseoyoung0402@gmail.com>
2021-03-10 22:38:24 +0900

    Create : CommentVC

---
+++ DNA/DNA/Comment/GCommentVC.swift
@@ -0,0 +1,29 @@
+//
+//  GCommentVC.swift
+//  DNA
+//
+//  Created by 장서영 on 2021/03/10.
+//
+
+import UIKit
+
+class GCommentVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
