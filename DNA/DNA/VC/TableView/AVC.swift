6a4585f7a7cf74410d99564968b39c8da87334dd
jangseoyoung <jangseoyoung0402@gmail.com>
2021-03-18 19:51:54 +0900

    Update : AVC

--- DNA/DNA/VC/TableView/AVC.swift
+++ DNA/DNA/VC/TableView/AVC.swift
@@ -6,13 +6,29 @@
 //
 
 import UIKit
+import Alamofire
 
-class AVC: UIViewController {
+class AVC: UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    @IBOutlet weak var tableView: UITableView!
 
     override func viewDidLoad() {
         super.viewDidLoad()
-
         // Do any additional setup after loading the view.
+        
+        
+        tableView.dataSource = self
+        tableView.delegate = self
+
+        
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        <#code#>
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ACell") as! ACell
     }
     
 
