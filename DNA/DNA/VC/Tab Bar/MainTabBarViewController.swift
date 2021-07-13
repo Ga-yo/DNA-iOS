832bcd8da3802be50add4a7f634d4af374ff40bf
jangseoyoung <jangseoyoung0402@gmail.com>
2021-07-13 16:15:29 +0900

    Create : VC

---
+++ DNA/DNA/VC/Tab Bar/MainTabBarViewController.swift
@@ -0,0 +1,17 @@
+//
+//  MainTabBarViewController.swift
+//  DNA
+//
+//  Created by 장서영 on 2021/06/29.
+//
+
+import UIKit
+
+class MainTabBarViewController: UITabBarController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setNavigationBar()
+        // Do any additional setup after loading the view.
+    }
+}
