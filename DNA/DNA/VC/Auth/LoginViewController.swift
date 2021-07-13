9a96c39e2d180e0e648047bffb4b58da1cb649ec
jangseoyoung <jangseoyoung0402@gmail.com>
2021-07-13 16:16:37 +0900

    Update : AuthViewController

--- DNA/DNA/VC/Auth/LoginViewController.swift
+++ DNA/DNA/VC/Auth/LoginViewController.swift
@@ -8,59 +8,64 @@
 import UIKit
 import Alamofire
 
-class LoginViewController: UIViewController {
-
-    @IBOutlet weak var circle: UIView!
-    @IBOutlet weak var emailTxt: UITextField!
-    @IBOutlet weak var pwTxt: UITextField!{
+final class LoginViewController: UIViewController {
+    
+    @IBOutlet weak private var circle: UIView!
+    @IBOutlet weak private var emailTxt: UITextField!
+    @IBOutlet weak private var pwTxt: UITextField!{
         didSet{
             pwTxt.isSecureTextEntry = true
         }
     }
-    @IBOutlet weak var warningLabel : UILabel!
-    @IBOutlet weak var signInButton: UIButton!
-    
-    let httpClient = HTTPClient()
+    @IBOutlet weak private var warningLabel : UILabel!
+    @IBOutlet weak private var signInButton: UIButton!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         setNavigationBar()
+        emailTxt.layer.sublayerTransform = CATransform3DMakeTranslation(20, 0, 0)
+        pwTxt.layer.sublayerTransform = CATransform3DMakeTranslation(20, 0, 0)
     }
     
-    @IBAction func signInButton(_ sender: UIButton!){
+    @IBAction private func signInButton(_ sender: UIButton!){
         SignIn(email: emailTxt.text!, password: pwTxt.text!)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
+        view.endEditing(true)
     }
     
-    func SignIn(email: String, password: String){
+    private func SignIn(email: String, password: String){
         print("로그인")
-        httpClient.post(url: AuthAPI.Login.path(), params: ["email":email, "password":password], header: Header.tokenIsEmpty.header()).responseJSON(completionHandler: {(response) in
+        HTTPClient().post(url: AuthAPI.Login.path(), params: ["email":email, "password":password], header: Header.tokenIsEmpty.header()).responseJSON(completionHandler: {[unowned self] response in
             switch response.response?.statusCode {
             case 201:
-                print("로그인 성공")
-                guard let data = response.data else {return}
-                guard let model = try? JSONDecoder().decode(SignInModel.self, from: data) else {return}
-                guard let pushVC = self.storyboard?.instantiateViewController(identifier: "MenuVC") as? MenuViewController else {return}
-                self.navigationController?.pushViewController(pushVC, animated: true)
+                let model = try? JSONDecoder().decode(SignInModel.self, from: response.data!)
+                Token.token = model!.access_token
+                
+                let sub = UIStoryboard(name: "Main", bundle: nil)
+                let main = sub.instantiateViewController(withIdentifier: "MainTabBarViewController") as! MainTabBarViewController
+                navigationController?.setViewControllers([main], animated: false)
             case 400:
                 print("BAD REQUEST")
+                errorAlert()
             case 401:
                 print("UNAUTHORIZED")
+                errorAlert()
             case 403:
                 print("FORBIDDEN")
+                errorAlert()
             case 404:
                 print("NOT FOUND")
+                errorAlert()
             case 409:
                 print("CONFLICT")
+                errorAlert()
             default :
-                //self.warningLabel.isHidden = false
                 print(response.response?.statusCode)
+                errorAlert()
             }
         })
     }
-
+    
 }
--- DNA/DNA/VC/Auth/SignUpViewController.swift
+++ DNA/DNA/VC/Auth/SignUpViewController.swift
@@ -8,84 +8,93 @@
 import UIKit
 import Alamofire
 
-class SignUpViewController: UIViewController {
+final class SignUpViewController: UIViewController {
 
-    @IBOutlet weak var circle: UIView!
-    @IBOutlet weak var nameTxt: UITextField!
-    @IBOutlet weak var emailTxt: UITextField!
-    @IBOutlet weak var confirmEmailButton: UIButton!
-    @IBOutlet weak var warningLabel: UILabel!
-    @IBOutlet weak var pwTxt: UITextField!{
+    @IBOutlet weak private var circle: UIView!
+    @IBOutlet weak private var nameTxt: UITextField!
+    @IBOutlet weak private var emailTxt: UITextField!
+    @IBOutlet weak private var confirmEmailButton: UIButton!
+    @IBOutlet weak private var warningLabel: UILabel!
+    @IBOutlet weak private var pwTxt: UITextField!{
         didSet {
             pwTxt.isSecureTextEntry = true
         }
     }
-    @IBOutlet weak var confirmPwTxt: UITextField!{
+    @IBOutlet weak private var confirmPwTxt: UITextField!{
         didSet {
             confirmPwTxt.isSecureTextEntry = true
         }
     }
-    @IBOutlet weak var signUpButton: UIButton!
+    @IBOutlet weak private var signUpButton: UIButton!
     
-    let id = String()
-    let httpclient = HTTPClient()
+    private let id = String()
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
         setNavigationBar()
+        emailTxt.layer.sublayerTransform = CATransform3DMakeTranslation(20, 0, 0)
+        nameTxt.layer.sublayerTransform = CATransform3DMakeTranslation(20, 0, 0)
+        confirmPwTxt.layer.sublayerTransform = CATransform3DMakeTranslation(20, 0, 0)
+        pwTxt.layer.sublayerTransform = CATransform3DMakeTranslation(20, 0, 0)
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        self.view.endEditing(true)
+        view.endEditing(true)
     }
     
-    @IBAction func confirmEmailButton(_ sender: UIButton){
-        print("tapped")
-        httpclient.get(url: AuthAPI.email(id).path(), params: nil, header: Header.tokenIsEmpty.header()).responseJSON(completionHandler: {
-            response in
+    @IBAction private func confirmEmailButton(_ sender: UIButton){
+        HTTPClient().get(url: AuthAPI.email(id).path(), params: nil, header: Header.tokenIsEmpty.header()).responseJSON(completionHandler: {
+            [unowned self] response in
             switch response.response?.statusCode {
             case 200 :
                 print("SUCCESS")
-                self.confirmEmailButton.setTitle("확인완료", for: .normal)
+                confirmEmailButton.setTitle("확인완료", for: .normal)
             case 400: print("BAD REQUEST")
-                self.confirmEmailButton.setTitle("확인불가", for: .normal)
+                confirmEmailButton.setTitle("확인불가", for: .normal)
             default : print(response.error ?? 0)
+                errorAlert()
             }
         })
     }
     
-    @IBAction func signUpButton(_ sender: UIButton){
+    @IBAction private func signUpButton(_ sender: UIButton){
         guard let Name = nameTxt.text else {return}
         guard let Email = emailTxt.text else {return}
         guard let Password = pwTxt.text else {return}
         signUp(name: Name, email: Email, password: Password)
     }
     
-    func signUp(name: String, email: String, password: String){
-        httpclient.post(url: AuthAPI.signUp.path(), params: ["name":name, "email":email, "password":password], header: Header.tokenIsEmpty.header()).responseJSON(completionHandler: {
-            response in
+    private func signUp(name: String, email: String, password: String){
+        HTTPClient().post(url: AuthAPI.signUp.path(), params: ["name":name, "email":email, "password":password], header: Header.tokenIsEmpty.header()).responseJSON(completionHandler: {
+            [unowned self] response in
             switch response.response?.statusCode {
             case 201:
                 print("SUCCESS")
-                self.navigationController?.popViewController(animated: true)
+                navigationController?.popViewController(animated: true)
             case 400:
-                self.warningLabel.isHidden = false
+                warningLabel.isHidden = false
                 print("BAD REQUEST")
+                errorAlert()
             case 401:
-                self.warningLabel.isHidden = false
+                warningLabel.isHidden = false
                 print("UNAUTHORIZED")
+                errorAlert()
             case 403:
-                self.warningLabel.isHidden = false
+                warningLabel.isHidden = false
                 print("FORBIDDEN")
+                errorAlert()
             case 404:
-                self.warningLabel.isHidden = false
+                warningLabel.isHidden = false
                 print("NOT FOUND")
+                errorAlert()
             case 409:
-                self.warningLabel.isHidden = false
+                warningLabel.isHidden = false
                 print("CONFLICT")
+                errorAlert()
             default :
+                warningLabel.isHidden = false
                 print(response.response?.statusCode ?? 0)
+                errorAlert()
             }
         })
     }
